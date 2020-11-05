661c64a62641ae34fa0b04b75dc0d5f94dbf3549
emiri.shimizu <sumire32511@gmail.com>
2020-11-05 12:25:59 +0000

    add img

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_223655) do
+ActiveRecord::Schema.define(version: 2020_11_05_100012) do
 
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_11_03_223655) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "img"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 

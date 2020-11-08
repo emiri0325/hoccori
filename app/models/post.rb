1b106a55c5429169510964f132a3c10a961a377e
emiri.shimizu <sumire32511@gmail.com>
2020-11-08 08:23:24 +0000

    delete liked / br / domain

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+  # お気に入り機能追加
+  has_many :favorites, dependent: :destroy
+  has_many :users, through: :favorites
+  
   mount_uploader :img, ImgUploader
   
   validates :content, presence: true, length: { maximum: 255 }
